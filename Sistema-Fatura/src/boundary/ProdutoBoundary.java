4a3b33309d321471a0163fc15db88b3f35330dbe
tiagolss <t.i.ago@live.com>
2015-11-22 14:05:57 -0200

    inclui campos na tela produto

--- Sistema-Fatura/src/boundary/ProdutoBoundary.java
+++ Sistema-Fatura/src/boundary/ProdutoBoundary.java
@@ -6,10 +6,25 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
+import javax.swing.JTextArea;
+import java.awt.Color;
+import javax.swing.JTable;
+import javax.swing.border.LineBorder;
+import javax.swing.JButton;
 
 public class ProdutoBoundary extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -32,11 +47,228 @@ public class ProdutoBoundary extends JFrame {
 	 */
 	public ProdutoBoundary() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 641, 444);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblIdDoProduto = new JLabel("ID do produto : *");
+		GridBagConstraints gbc_lblIdDoProduto = new GridBagConstraints();
+		gbc_lblIdDoProduto.insets = new Insets(0, 0, 5, 5);
+		gbc_lblIdDoProduto.anchor = GridBagConstraints.EAST;
+		gbc_lblIdDoProduto.gridx = 0;
+		gbc_lblIdDoProduto.gridy = 0;
+		contentPane.add(lblIdDoProduto, gbc_lblIdDoProduto);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.gridwidth = 2;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblDescrio = new JLabel("Descri\u00E7\u00E3o : *");
+		GridBagConstraints gbc_lblDescrio = new GridBagConstraints();
+		gbc_lblDescrio.anchor = GridBagConstraints.EAST;
+		gbc_lblDescrio.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDescrio.gridx = 0;
+		gbc_lblDescrio.gridy = 1;
+		contentPane.add(lblDescrio, gbc_lblDescrio);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.gridwidth = 4;
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 1;
+		contentPane.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblPreo = new JLabel("Pre\u00E7o : *");
+		GridBagConstraints gbc_lblPreo = new GridBagConstraints();
+		gbc_lblPreo.anchor = GridBagConstraints.EAST;
+		gbc_lblPreo.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPreo.gridx = 0;
+		gbc_lblPreo.gridy = 2;
+		contentPane.add(lblPreo, gbc_lblPreo);
+		
+		textField_2 = new JTextField();
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridx = 1;
+		gbc_textField_2.gridy = 2;
+		contentPane.add(textField_2, gbc_textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblIvgv = new JLabel("imposto : *");
+		GridBagConstraints gbc_lblIvgv = new GridBagConstraints();
+		gbc_lblIvgv.insets = new Insets(0, 0, 5, 5);
+		gbc_lblIvgv.anchor = GridBagConstraints.EAST;
+		gbc_lblIvgv.gridx = 2;
+		gbc_lblIvgv.gridy = 2;
+		contentPane.add(lblIvgv, gbc_lblIvgv);
+		
+		textField_3 = new JTextField();
+		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+		gbc_textField_3.gridwidth = 2;
+		gbc_textField_3.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_3.gridx = 3;
+		gbc_textField_3.gridy = 2;
+		contentPane.add(textField_3, gbc_textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblObservao = new JLabel("Observa\u00E7\u00E3o : *");
+		GridBagConstraints gbc_lblObservao = new GridBagConstraints();
+		gbc_lblObservao.anchor = GridBagConstraints.NORTH;
+		gbc_lblObservao.insets = new Insets(0, 0, 5, 5);
+		gbc_lblObservao.gridx = 0;
+		gbc_lblObservao.gridy = 3;
+		contentPane.add(lblObservao, gbc_lblObservao);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setTabSize(7);
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.gridheight = 2;
+		gbc_textArea.insets = new Insets(0, 0, 5, 5);
+		gbc_textArea.gridwidth = 4;
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 1;
+		gbc_textArea.gridy = 3;
+		contentPane.add(textArea, gbc_textArea);
+		
+		JLabel lblCamposObrigatrios = new JLabel("* Campos obrigat\u00F3rios");
+		lblCamposObrigatrios.setForeground(Color.BLUE);
+		GridBagConstraints gbc_lblCamposObrigatrios = new GridBagConstraints();
+		gbc_lblCamposObrigatrios.gridwidth = 2;
+		gbc_lblCamposObrigatrios.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCamposObrigatrios.gridx = 0;
+		gbc_lblCamposObrigatrios.gridy = 5;
+		contentPane.add(lblCamposObrigatrios, gbc_lblCamposObrigatrios);
+		
+		table = new JTable();
+		table.setBorder(new LineBorder(new Color(0, 0, 0)));
+		GridBagConstraints gbc_table = new GridBagConstraints();
+		gbc_table.gridheight = 3;
+		gbc_table.gridwidth = 5;
+		gbc_table.insets = new Insets(0, 0, 5, 5);
+		gbc_table.fill = GridBagConstraints.BOTH;
+		gbc_table.gridx = 0;
+		gbc_table.gridy = 7;
+		contentPane.add(table, gbc_table);
+		
+		JPanel panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.fill = GridBagConstraints.VERTICAL;
+		gbc_panel.gridwidth = 2;
+		gbc_panel.gridheight = 3;
+		gbc_panel.insets = new Insets(0, 0, 0, 5);
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 10;
+		contentPane.add(panel, gbc_panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JButton btnPrimeiro = new JButton("Primeiro");
+		GridBagConstraints gbc_btnPrimeiro = new GridBagConstraints();
+		gbc_btnPrimeiro.insets = new Insets(0, 0, 0, 5);
+		gbc_btnPrimeiro.gridx = 0;
+		gbc_btnPrimeiro.gridy = 1;
+		panel.add(btnPrimeiro, gbc_btnPrimeiro);
+		
+		JButton btnAnterior = new JButton("Anterior");
+		GridBagConstraints gbc_btnAnterior = new GridBagConstraints();
+		gbc_btnAnterior.insets = new Insets(0, 0, 0, 5);
+		gbc_btnAnterior.gridx = 1;
+		gbc_btnAnterior.gridy = 1;
+		panel.add(btnAnterior, gbc_btnAnterior);
+		
+		JButton btnPrximo = new JButton("Pr\u00F3ximo");
+		GridBagConstraints gbc_btnPrximo = new GridBagConstraints();
+		gbc_btnPrximo.insets = new Insets(0, 0, 0, 5);
+		gbc_btnPrximo.gridx = 2;
+		gbc_btnPrximo.gridy = 1;
+		panel.add(btnPrximo, gbc_btnPrximo);
+		
+		JButton btnNewButton = new JButton("Ultimo");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.gridx = 3;
+		gbc_btnNewButton.gridy = 1;
+		panel.add(btnNewButton, gbc_btnNewButton);
+		
+		JPanel panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.gridheight = 3;
+		gbc_panel_1.gridwidth = 3;
+		gbc_panel_1.insets = new Insets(0, 0, 0, 5);
+		gbc_panel_1.gridx = 2;
+		gbc_panel_1.gridy = 10;
+		contentPane.add(panel_1, gbc_panel_1);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[] {0, 0, 0, 0, 0, 0};
+		gbl_panel_1.rowHeights = new int[]{0, 0, 0};
+		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panel_1.setLayout(gbl_panel_1);
+		
+		JButton btnNovo = new JButton("Novo");
+		GridBagConstraints gbc_btnNovo = new GridBagConstraints();
+		gbc_btnNovo.gridwidth = 2;
+		gbc_btnNovo.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNovo.gridx = 0;
+		gbc_btnNovo.gridy = 0;
+		panel_1.add(btnNovo, gbc_btnNovo);
+		
+		JButton btnEditar = new JButton("Editar");
+		GridBagConstraints gbc_btnEditar = new GridBagConstraints();
+		gbc_btnEditar.insets = new Insets(0, 0, 5, 5);
+		gbc_btnEditar.gridx = 2;
+		gbc_btnEditar.gridy = 0;
+		panel_1.add(btnEditar, gbc_btnEditar);
+		
+		JButton btnSalvar = new JButton("Salvar");
+		GridBagConstraints gbc_btnSalvar = new GridBagConstraints();
+		gbc_btnSalvar.gridwidth = 2;
+		gbc_btnSalvar.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSalvar.gridx = 0;
+		gbc_btnSalvar.gridy = 1;
+		panel_1.add(btnSalvar, gbc_btnSalvar);
+		
+		JButton btnExcluir = new JButton("Excluir");
+		GridBagConstraints gbc_btnExcluir = new GridBagConstraints();
+		gbc_btnExcluir.insets = new Insets(0, 0, 0, 5);
+		gbc_btnExcluir.gridx = 2;
+		gbc_btnExcluir.gridy = 1;
+		panel_1.add(btnExcluir, gbc_btnExcluir);
+		
+		JButton btnPesquisa = new JButton("Pesquisa");
+		GridBagConstraints gbc_btnPesquisa = new GridBagConstraints();
+		gbc_btnPesquisa.insets = new Insets(0, 0, 0, 5);
+		gbc_btnPesquisa.gridx = 3;
+		gbc_btnPesquisa.gridy = 1;
+		panel_1.add(btnPesquisa, gbc_btnPesquisa);
+		
+		JLabel lblNewLabel = new JLabel("");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 11;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
 	}
 
 }
